f61f379c8c2b3255e88ed0e74f52ae8545168b50
Maroua Bouanz <maroua.bouanz@gmail.com>
2023-09-18 10:22:01 +0100

    Communication using props

--- src/LoanForm.js
+++ src/LoanForm.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./FormStyle.css";
 import Module from "./Module";
+import MyComponent from "./MyComponent";
+import SecondComponent from "./SecondComponent";
+import ThirdComponent from "./ThirdComponent";
 import { useState } from "react";
+
 function LoanForm() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [showModal, setShowModal] = useState(false);
@@ -37,6 +41,25 @@ function LoanForm() {
     loanInputs.phoneNumber == "" ||
     loanInputs.age == "";
 
+  function handlePhoneNumberInputChange(value) {
+    setLoanInputs({ ...loanInputs, phoneNumber: value });
+  }
+
+  function handleNameInputChange(value) {
+    setLoanInputs({ ...loanInputs, name: value });
+  }
+
+  function handleAgeInputChange(value) {
+    setLoanInputs({ ...loanInputs, age: value });
+  }
+
+  function handleEmployeeInputChange(checked) {
+    setLoanInputs({ ...loanInputs, isEmployee: checked });
+  }
+  function handleSalaryInputChange(value) {
+    setLoanInputs({ ...loanInputs, salaryRange: value });
+  }
+
   return (
     <div
       onClick={handleDivClick}
@@ -46,36 +69,66 @@ function LoanForm() {
       <form className="flex" style={{ flexDirection: "column" }} id="loan-form">
         <h1>Requesting a loan</h1>
         <hr />
-        <label> Name:</label>
+        {/* <label> Name:</label>
         <input
           value={loanInputs.name}
           onChange={(e) => {
             setLoanInputs({ ...loanInputs, name: e.target.value });
           }}
+        /> */}
+
+        <MyComponent
+          value={loanInputs.name}
+          handleChange={handleNameInputChange}
+          inputName="Name"
         />
-        <label> Phone Number:</label>
+        <MyComponent
+          value={loanInputs.phoneNumber}
+          handleChange={handlePhoneNumberInputChange}
+          inputName="Phone Number"
+        />
+
+        <MyComponent
+          value={loanInputs.age}
+          handleChange={handleAgeInputChange}
+          inputName="Age"
+        />
+
+        <SecondComponent
+          checked={loanInputs.isEmployee}
+          handleChange={handleEmployeeInputChange}
+          inputName="Are you an Employee?"
+        />
+
+        <ThirdComponent
+          value={loanInputs.salaryRange}
+          handleChange={handleSalaryInputChange}
+          inputName="SalaryRange"
+        />
+
+        {/* <label> Phone Number:</label>
         <input
           value={loanInputs.phoneNumber}
           onChange={(e) => {
             setLoanInputs({ ...loanInputs, phoneNumber: e.target.value });
           }}
-        />
-        <label> Age:</label>
+        /> */}
+        {/* <label> Age:</label>
         <input
           value={loanInputs.age}
           onChange={(e) => {
             setLoanInputs({ ...loanInputs, age: e.target.value });
           }}
-        />
-        <label style={{ marginTop: "30px" }}>Are you an Employee?</label>
+        /> */}
+        {/* <label style={{ marginTop: "30px" }}>Are you an Employee?</label>
         <input
           type="checkbox"
           checked={loanInputs.isEmployee}
           onChange={(e) => {
             setLoanInputs({ ...loanInputs, isEmployee: e.target.checked });
-          }}
-        />
-        <label>Salary</label>
+          // }}
+        /> */}
+        {/* <label>Salary</label>
         <select
           value={loanInputs.salaryRange}
           onChange={(e) => {
@@ -85,7 +138,7 @@ function LoanForm() {
           <option>Less Than 500$</option>
           <option>Between 500$ and 2000$</option>
           <option>Above 2000$</option>
-        </select>
+        </select> */}
 
         <button
           id="submit-loan-btn"
